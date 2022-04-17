08d797ef7ed46063a1ae552170daa29655317d06
nikoprl <45226198+nikoprl@users.noreply.github.com>
2022-04-17 14:44:11 +0300

    hello world pwa

---
+++ sw.js
@@ -0,0 +1,30 @@
+var cacheName = "hello-pwa";
+var filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    (async () => {
+      try {
+        const cache = await caches.open(cacheName);
+        return cache.addAll(filesToCache);
+      } catch (e) {
+        console.log("after install", e.message);
+      }
+    })()
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      try {
+        const response = await caches.match(e.request);
+        return response || fetch(e.request);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })()
+  );
+});
